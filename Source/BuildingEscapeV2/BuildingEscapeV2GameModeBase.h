884a5f0d229ba792cf1793b7d260100fd032bd45
Soumya Ranjan Parida <soumyafc812@gmail.com>
2021-10-24 09:44:33 +0530

    All project files added

---
+++ Source/BuildingEscapeV2/BuildingEscapeV2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEscapeV2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPEV2_API ABuildingEscapeV2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
